88a4211a8905507705b271a4b23ba56d5e85a961
Aliaksandr91 <horbachalex91@gmail.com>
2023-12-04 20:27:23 +0100

    feat: add reducers for tasksReducers RTK

--- src/features/TodolistsList/tasks-reducer.ts
+++ src/features/TodolistsList/tasks-reducer.ts
@@ -3,13 +3,38 @@ import { Dispatch } from "redux"
 import { AppThunk } from "app/store"
 import { appActions } from "app/app-reducer"
 import { handleServerAppError, handleServerNetworkError } from "utils/error-utils"
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { todolistsActions } from "features/TodolistsList/todolists-reducer"
 
 const slice = createSlice({
   name: "tasks",
   initialState: {} as TasksStateType,
-  reducers: {},
+  reducers: {
+    removeTask: (state, action: PayloadAction<{ taskId: string; todolistId: string }>) => {
+      const tasksForTodolist = state[action.payload.todolistId]
+      const index = tasksForTodolist.findIndex((task) => task.id === action.payload.taskId)
+      if (index !== -1) {
+        tasksForTodolist.slice(index, 1)
+      }
+    },
+    addTask: (state, action: PayloadAction<{ task: TaskType }>) => {
+      const tasksForTodolist = state[action.payload.task.todoListId]
+      tasksForTodolist.unshift(action.payload.task)
+    },
+    updateTask: (
+      state,
+      action: PayloadAction<{ taskId: string; model: UpdateDomainTaskModelType; todolistId: string }>,
+    ) => {
+      const tasksForTodolist = state[action.payload.todolistId]
+      const index = tasksForTodolist.findIndex((task) => task.id === action.payload.taskId)
+      if (index !== -1) {
+        tasksForTodolist[index] = { ...tasksForTodolist[index], ...action.payload.model }
+      }
+    },
+    setTasks: (state, action: PayloadAction<{ tasks: TaskType[]; todolistId: string }>) => {
+      state[action.payload.todolistId] = action.payload.tasks
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(todolistsActions.addTodolist, (state, action) => {
@@ -29,45 +54,8 @@ export const tasksReducer = slice.reducer
 export const tasksActions = slice.actions
 const initialState: TasksStateType = {}
 
-export const _tasksReducer = (state: TasksStateType = initialState, action: ActionsType): TasksStateType => {
-  switch (action.type) {
-    case "REMOVE-TASK":
-      return { ...state, [action.todolistId]: state[action.todolistId].filter((t) => t.id != action.taskId) }
-    case "ADD-TASK":
-      return { ...state, [action.task.todoListId]: [action.task, ...state[action.task.todoListId]] }
-    case "UPDATE-TASK":
-      return {
-        ...state,
-        [action.todolistId]: state[action.todolistId].map((t) =>
-          t.id === action.taskId ? { ...t, ...action.model } : t,
-        ),
-      }
-    case "ADD-TODOLIST":
-      return { ...state, [action.todolist.id]: [] }
-    case "REMOVE-TODOLIST":
-      const copyState = { ...state }
-      delete copyState[action.id]
-      return copyState
-    case "SET-TODOLISTS": {
-      const copyState = { ...state }
-      action.todolists.forEach((tl: any) => {
-        copyState[tl.id] = []
-      })
-      return copyState
-    }
-    case "SET-TASKS":
-      return { ...state, [action.todolistId]: action.tasks }
-    default:
-      return state
-  }
-}
-
 // actions
-export const removeTaskAC = (taskId: string, todolistId: string) =>
-  ({ type: "REMOVE-TASK", taskId, todolistId }) as const
-export const addTaskAC = (task: TaskType) => ({ type: "ADD-TASK", task }) as const
-export const updateTaskAC = (taskId: string, model: UpdateDomainTaskModelType, todolistId: string) =>
-  ({ type: "UPDATE-TASK", model, todolistId, taskId }) as const
+
 export const setTasksAC = (tasks: Array<TaskType>, todolistId: string) =>
   ({ type: "SET-TASKS", tasks, todolistId }) as const
 
@@ -156,9 +144,4 @@ export type UpdateDomainTaskModelType = {
 export type TasksStateType = {
   [key: string]: Array<TaskType>
 }
-type ActionsType =
-  | ReturnType<typeof removeTaskAC>
-  | ReturnType<typeof addTaskAC>
-  | ReturnType<typeof updateTaskAC>
-  | ReturnType<typeof setTasksAC>
-  | any
+type ActionsType = ReturnType<typeof updateTaskAC> | ReturnType<typeof setTasksAC> | any
